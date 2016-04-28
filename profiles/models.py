ea8f099c641f199e05e4a9017e2de33415a46afd
Markus Sommer <markus@splork.de>
2016-04-28 20:26:22 +0200

    Changed profile-model

--- profiles/models.py
+++ profiles/models.py
@@ -3,8 +3,13 @@ from django.contrib.auth.models import User
 
 
 class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    # The user that this profile is associated with
+    # Since each profile is associated with exactly one user, the user is the profiles primary key
+    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
+    # Whether or not the users true identity has been verified
     verified = models.BooleanField(default=False)
+    # The number of points the user has gained/lost
+    points = models.IntegerField(default=0)
 
     def __str__(self):
         return self.user.__str__()
