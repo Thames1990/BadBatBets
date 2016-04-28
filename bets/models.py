561c65af3ba22932411138b011ea685aeba90646
Markus Sommer <markus@splork.de>
2016-04-28 20:50:46 +0200

    Created Models: Bet (abstract) BinaryBet

--- bets/models.py
+++ bets/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
-# Create your models here.
+from profiles.models import Profile
+
+
+class Bet(models.Model):
+    """Abstract superclass for fields which all bets have in common"""
+    class Meta:
+        abstract = True
+
+    # Each bet has a user who owns it (usually the user that created it)
+    owner = models.ForeignKey(Profile)
+    # Name to be shown in the index
+    name = models.CharField(max_length=64)
+    # Detailed description of the bet
+    description = models.TextField()
+    # Date+Time when the bet was created
+    created = models.DateTimeField(auto_now_add=True)
+    # Date+Time when the bet is published
+    pub_date = models.DateTimeField()
+
+    def __str__(self):
+        return self.name
+
+
+class BinaryBet(Bet):
+    """Bet which has a binary outcome"""
+
+    # One of the two possible outcomes
+    null_option = models.CharField(max_length=64)
+    # The other possible outcome
+    alternative_option = models.CharField(max_length=64)
--- the_platform/settings.py
+++ the_platform/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'profiles.apps.ProfilesConfig',
+    'bets.apps.BetsConfig',
 ]
 
 MIDDLEWARE_CLASSES = [
