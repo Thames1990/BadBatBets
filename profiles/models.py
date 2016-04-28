b7dc35c6afb0e568221b79ef3338d9bcf1028e99
Markus Sommer <markus@splork.de>
2016-04-28 20:19:10 +0200

    Registered profile-model with the admin interface.

--- profiles/models.py
+++ profiles/models.py
@@ -5,3 +5,6 @@ from django.contrib.auth.models import User
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     verified = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.user.__str__()
