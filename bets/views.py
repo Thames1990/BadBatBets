5daba9027078eec478bf4455c6203df202dc4ac3
Thames1990 <ThomasMohr1990@googlemail.com>
2016-05-01 19:24:18 +0200

    Added preparations for POST bet system

--- bets/views.py
+++ bets/views.py
@@ -21,3 +21,17 @@ def placed_binary_bet(request, binary_bet_prim_key_, placed_binary_bet_prim_key)
     bet = get_object_or_404(BinaryBet, prim_key=binary_bet_prim_key_)
     placed_bet = get_object_or_404(PlacedBinaryBet, prim_key=placed_binary_bet_prim_key)
     return render(request, 'placed_binary_bet.html', {'placed_bet': placed_bet})
+
+
+def bet_on_binary_bet(request, prim_key):
+    # TODO Do stuff Markus
+    bet = get_object_or_404(BinaryBet, prim_key=prim_key)
+    # try:
+    #     # Get choice
+    # except (KeyError, Choice.DoesNotExist):
+    #     return render(request, 'binary_bet.html', {
+    #         'bet': bet,
+    #         'error_message': "You didn't select a valid choice."
+    #     })
+    # else:
+    #     # Select choice, save stuff and return to site
