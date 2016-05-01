08406c83bdd97d7365b0a9a0d2793bef83c986cc
Thames1990 <ThomasMohr1990@googlemail.com>
2016-05-01 17:30:52 +0200

    Added placed bets to HTML

--- bets/urls.py
+++ bets/urls.py
@@ -7,5 +7,7 @@ urlpatterns = [
     # /bets/
     url(r'^$', views.index, name='index'),
     # /bets/<prim_key>
-    url(r'^(?P<prim_key>[0-9]+)/$', views.bets, name='detail'),
+    url(r'^(?P<prim_key>[0-9]+)/$', views.binary_bet, name='binary_bet'),
+    # /bets/<prim_key>/placed/
+    url(r'^(?P<prim_key>[0-9]+)/placed/$', views.placed_binary_bet, name='placed_binary_bet'),
 ]
--- bets/views.py
+++ bets/views.py
@@ -1,12 +1,22 @@
 from django.shortcuts import render, get_object_or_404
-from .models import BinaryBet
+from .models import BinaryBet, PlacedBinaryBet
 
 
 def index(request):
     all_binary_bets = BinaryBet.objects.all()
-    return render(request, 'index.html', {'all_binary_bets': all_binary_bets})
+    all_placed_bets = PlacedBinaryBet.objects.all()
+    context = {
+        'all_binary_bets': all_binary_bets,
+        'all_placed_bets': all_placed_bets
+    }
+    return render(request, 'index.html', context)
 
 
-def bets(request, prim_key):
+def binary_bet(request, prim_key):
     bet = get_object_or_404(BinaryBet, prim_key=prim_key)
-    return render(request, 'bets.html', {'bet': bet})
+    return render(request, 'binary_bet.html', {'bet': bet})
+
+
+def placed_binary_bet(request, prim_key):
+    placed_bet = get_object_or_404(PlacedBinaryBet, prim_key=prim_key)
+    return render(request, 'placed_binary_bet.html', {'placed_bet': placed_bet})
