a112f27b7b2255710ea14bbd40a2b833a0668cd7
Markus Sommer <markus@splork.de>
2016-05-01 20:59:56 +0200

    Users can now be logged in.

--- profiles/urls.py
+++ profiles/urls.py
@@ -5,5 +5,6 @@ from . import views
 app_name = 'profiles'
 urlpatterns = [
     url(r'^$', views.profile, name='profile'),
-    url(r'login/$', views.login, name='login'),
+    url(r'^login/$', views.login_page, name='login'),
+    url(r'^login/login/$', views.login_user, name='login_post')
 ]
--- profiles/views.py
+++ profiles/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.contrib.auth import authenticate, login
 
 
 def landing(request):
@@ -13,11 +14,30 @@ def profile(request):
     if request.user.is_authenticated():
         return HttpResponse("Logged in!")
     else:
-        return redirect('profiles:login')
+        return redirect('profiles:login_page')
 
 
-def login(request):
+def login_page(request):
     if request.user.is_authenticated():
         return redirect('profiles:profile')
     else:
-        return HttpResponse("Login Page")
+        return render(request, 'profiles/login.html')
+
+
+def login_user(request):
+    username = request.POST['username']
+    password = request.POST['password']
+    user = authenticate(username=username, password=password)
+
+    if user is not None:
+        if user.is_active:
+            login(request, user)
+            return redirect('profiles:profile')
+        else:
+            return render(request, 'profiles/login.html', {
+                'error_message': "Account inactive"
+            })
+    else:
+        return render(request, 'profiles/login.html', {
+                'error_message': "Unknown User"
+            })
