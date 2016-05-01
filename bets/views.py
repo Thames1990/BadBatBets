1242cf3a6ee98be8bbe8e34cd99e3e2d579f42c3
Thames1990 <ThomasMohr1990@googlemail.com>
2016-05-01 12:28:37 +0200

    Refactored templates Refined bet overview

--- bets/views.py
+++ bets/views.py
@@ -5,7 +5,7 @@ from .models import BinaryBet
 
 def index(request):
     all_binary_bets = BinaryBet.objects.all()
-    return render(request, 'bets/index.html', {'all_binary_bets': all_binary_bets})
+    return render(request, 'index.html', {'all_binary_bets': all_binary_bets})
 
 
 def bets(request, prim_key):
@@ -13,4 +13,4 @@ def bets(request, prim_key):
         bet = BinaryBet.objects.get(prim_key=prim_key)
     except BinaryBet.DoesNotExist:
         raise Http404("Bet doesn't exist")
-    return render(request, 'bets/bets.html', {'bet': bet})
+    return render(request, 'bets.html', {'bet': bet})
