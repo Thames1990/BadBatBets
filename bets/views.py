cfc73e1f680b9136ab2b6c60f810fe6827d57ca6
Thames1990 <ThomasMohr1990@googlemail.com>
2016-05-02 12:21:51 +0200

    Minor changes

--- bets/views.py
+++ bets/views.py
@@ -1,5 +1,6 @@
-from django.http import Http404
+from django.http import Http404, HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
+from django.core.urlresolvers import reverse
 
 from .models import BinaryBet, PlacedBinaryBet
 
@@ -36,12 +37,14 @@ def bet_on_binary_bet(request, prim_key):
     if request.user.is_authenticated():
         bet = get_object_or_404(BinaryBet, prim_key=prim_key)
         choice = request.POST['choice']
-        placed_amount = 5000
+        # TODO Add placed amount field
+        placed_amount = 9001
+        # TODO Improve choice system
         chose_alternative = "alternative" in choice
         placed_bet = PlacedBinaryBet(
             placed_on=bet,
             placed_by=request.user.profile,
-            placed_amount=1000,
+            placed_amount=placed_amount,
             chose_alternative=chose_alternative
         )
         placed_bet.save()
@@ -55,6 +58,7 @@ def bet_on_binary_bet(request, prim_key):
             'bet_message': "Successfully bet " + str(placed_amount) + " on " + bet_on + " for" + bet.name
         }
         return render(request, 'index.html', context)
-        # return HttpResponseRedirect(reverse('bets:binary_bet', args=(bet.prim_key,)))
+        # TODO Use HttpResponseRedirect
+        # return HttpResponseRedirect(reverse('index.html'))
     else:
         return render(request, 'profiles/login.html')
