78c3f6d284a725e1a83ef5b2782cdf8b1baaba6b
Markus Sommer <markus@splork.de>
2016-05-13 13:55:41 +0200

    Modified Bets-Models & customised admin-page

--- bets/admin.py
+++ bets/admin.py
@@ -1,7 +1,24 @@
 from django.contrib import admin
 
-from .models import Bet, Choice, ChoiceBet
+from .models import ChoiceBet, Choice, PlacedChoiceBet
 
-admin.site.register(Bet)
-admin.site.register(Choice)
-admin.site.register(ChoiceBet)
+
+class ChoiceInLine(admin.TabularInline):
+    model = Choice
+    extra = 2
+
+
+class ChoiceBetAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ('General', {'fields': ['name', 'description']}),
+        ('People', {'fields': ['owner', 'forbidden']}),
+        ('Dates', {'fields': ['pub_date', 'end_bets', 'end_date']}),
+        ('Misc', {
+            'fields': ['resolved'],
+            'classes': ('collapse',),
+        },),
+    ]
+    inlines = [ChoiceInLine]
+    list_display = ('name', 'created', 'pub_date', 'end_date', 'resolved')
+
+admin.site.register(ChoiceBet, ChoiceBetAdmin)
--- bets/models.py
+++ bets/models.py
@@ -1,3 +1,4 @@
+from django.utils import timezone
 from django.db import models
 
 from bets.util import pkgen
@@ -6,9 +7,8 @@ from profiles.models import Profile, ForbiddenUser
 
 class Bet(models.Model):
 
-    BET_TYPES = (
-        ('choice', 'Choices'),
-    )
+    class Meta:
+        abstract = True
 
     # Random key for each bet
     prim_key = models.PositiveIntegerField(primary_key=True, default=pkgen)
@@ -22,22 +22,31 @@ class Bet(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     # Date when the bet is published
     pub_date = models.DateField()
-    # Date when the bet will be closed (if it is not resolved before)
-    end_date = models.DateField(blank=True, null=True)
+    # After this, no further bets may be placed
+    end_bets = models.DateField(blank=True, null=True)
     # Whether or not the bet has been resolved
     resolved = models.BooleanField(default=False)
-    # Type of the bet
-    type = models.CharField(max_length=64, choices=BET_TYPES)
     # People that are not allowed to see this bet
     forbidden = models.ManyToManyField(ForbiddenUser)
 
     def __str__(self):
         return self.name
 
+    def open_to_bets(self):
+        if self.end_bets is None:
+            return True
+        else:
+            return timezone.now() < self.end_bets
+
+
+class ChoiceBet(Bet):
+    # Date when the bet will be closed (if it is not resolved before)
+    end_date = models.DateField(blank=True, null=True)
+
 
 class Choice(models.Model):
     # The bet to which this choice belongs
-    belongs_to = models.ForeignKey(Bet)
+    belongs_to = models.ForeignKey(ChoiceBet)
     # (Short) description of the choice
     description = models.CharField(max_length=64)
 
@@ -45,11 +54,11 @@ class Choice(models.Model):
         return self.belongs_to.name + ": " + self.description
 
 
-class ChoiceBet(models.Model):
+class PlacedChoiceBet(models.Model):
     # User that placed this bet
     placed_by = models.ForeignKey(Profile)
     # The bet it is placed on
-    placed_on = models.ForeignKey(Bet)
+    placed_on = models.ForeignKey(ChoiceBet)
     # Choice that the user selected
     chosen = models.ForeignKey(Choice)
     # Amount the user placed
