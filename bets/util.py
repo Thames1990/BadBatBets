97173b3b6f8b993ea76ac199424f6f3d5b8adb3b
Markus Sommer <markus@splork.de>
2016-05-15 18:26:26 +0200

    Method that finds the PlacedBet for a user.

--- bets/util.py
+++ bets/util.py
@@ -4,3 +4,26 @@ def pkgen():
     randomiser = SystemRandom()
     # 2147483647 is the largest values supported by django's PositiveIntegerField
     return randomiser.randint(0, 2147483647)
+
+
+def find_bet_for_user(bet, user):
+    """Finds a placed bet made by that user on that bet"""
+    from bets.models import ChoiceBet, DateBet
+    from profiles.models import Profile
+
+    profile = Profile.objects.get(pk=user)
+
+    if type(bet) == ChoiceBet:
+        bets = bet.placedchoicebet_set.filter(placed_by__exact=profile)
+    elif type(bet) == DateBet:
+        bets = bet.placeddatebet_set.filter(placed_by__exact=profile)
+    else:
+        return None
+
+    if len(bets) == 0:
+        return None
+    elif len(bets) > 1:
+        # TODO: Notify admins that something is wrong.
+        return bets[0]
+    else:
+        return bets[0]
