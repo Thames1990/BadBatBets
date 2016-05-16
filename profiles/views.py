69474301096c839c699a11d3738c91f97a7c4eda
Thames1990 <thomasmohr1990@gmail.com>
2016-05-16 13:41:49 +0200

    Fixed typo

--- profiles/views.py
+++ profiles/views.py
@@ -42,7 +42,7 @@ def login_user(request):
 def logout_user(request):
     logout(request)
     return render(request, 'profiles/login.html', {
-        'message': "Logout Successfull"
+        'message': "Logout Successful"
     })
 
 
