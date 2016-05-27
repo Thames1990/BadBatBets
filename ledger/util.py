4a02ec324f95044bde171a10fc406b96aebdd777
Markus Sommer <markus@splork.de>
2016-05-27 16:14:35 +0200

    Util function to transfer money on bet placement

--- ledger/util.py
+++ ledger/util.py
@@ -33,3 +33,55 @@ def create_table(credit, debit):
         table.append((entry.transaction.timestamp, "-", entry.amount))
 
     return sorted(table, key=lambda x: x[0])
+
+
+def place_bet_transaction(user, bet, amount):
+    from django.utils.translation import ugettext_lazy as _
+    from django.contrib.auth.models import User
+
+    from .models import Transaction, Debit, Credit
+    from .exceptions import InsufficientFunds
+    from bets.models import Bet
+    from profiles.models import Profile
+
+    assert isinstance(user, User) or isinstance(user, Profile)
+    if isinstance(user, User):
+        username = user.username
+        user = user.profile.account
+    else:
+        username = user.user.username
+        user = user.account
+
+    assert isinstance(bet, Bet)
+    bet_id = bet.id
+    bet = bet.account
+
+    if not user.balance >= amount:
+        raise InsufficientFunds(
+            _("User has insufficient funds"),
+            code='insufficient_funds_on_placement'
+        )
+
+    transaction = Transaction(
+        description="Placed Bet\nBet: " + str(bet_id) + "\nUser: " + username + "\nAmount: " + str(amount)
+    )
+    debit = Debit(
+        transaction=transaction,
+        account=user,
+        amount=amount
+    )
+    credit = Credit(
+        transaction=transaction,
+        account=bet,
+        amount=amount
+    )
+
+    transaction.save()
+    debit.save()
+    credit.save()
+
+    user.compute_balance()
+    bet.compute_balance()
+
+    return transaction
+
