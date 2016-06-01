5c24fd8dc6e79ee83056589748e842429a10b08c
Markus Sommer <markus@splork.de>
2016-06-01 16:42:39 +0200

    Bet resolutin finished (not tested yet, though...

--- bets/util.py
+++ bets/util.py
@@ -230,27 +230,16 @@ def place_bet_transaction(profile, bet, amount):
         raise
 
 
-# TODO Make generic. What's the type of a date (winning_option)?
-def resolve_choice_bet(bet, winning_option):
+def perform_payout(bet, winning_bets):
     """
-    Resolves a choice bet and distributes the pot among the user who placed a bet on the winning choice.
-    :param bet: Bet to be resolved
-    :param winning_option: The winning choice
+    Pays out the pot of a bet to the winning users
+    :param bet: A subtype of Bet
+    :param winning_bets: list of winning placed bets
+    :return: transaction detailing the payout (already saved)
     """
-    from .models import ChoiceBet, Choice
     from ledger.models import Account
     from ledger.util import one_to_many_transaction
     from ledger.exceptions import InsufficientFunds
-
-    assert isinstance(bet, ChoiceBet)
-    assert isinstance(winning_option, Choice)
-
-    placed_bets = bet.placedchoicebet_set.all()
-    winning_bets = []
-
-    for placed_bet in placed_bets:
-        if placed_bet.chosen == winning_option:
-            winning_bets.append(placed_bet)
 
     payout = int(bet.account.balance / len(winning_bets))
 
@@ -282,30 +271,45 @@ def resolve_choice_bet(bet, winning_option):
             code='i_dun_goofed'
         )
 
+    return transaction
+
+
+def resolve_bet(bet, winning_option):
+    """
+    Resolves a bet and performs the payout
+    :param bet: Either a ChoiceBet or a DateBet
+    :param winning_option: Either a Choice or datetime.date
+    :return: transaction detailing the payout
+    """
+    from .models import DateBet, ChoiceBet, Choice
+    from ledger.exceptions import InsufficientFunds
+    from datetime import date
+
+    assert isinstance(bet, DateBet) or isinstance(bet, ChoiceBet)
+
+    if isinstance(bet, DateBet):
+        assert isinstance(winning_option, date)
+
+        placed_bets = bet.placeddatebet_set.all()
+
+        winning_bets = find_winning_dates(placed_bets=placed_bets, winning_date=winning_option)
+
+    else:
+        assert isinstance(winning_option, Choice)
+
+        placed_bets = bet.placedchoicebet_set.all()
+
+        winning_bets = find_winning_choices(placed_bets=placed_bets, winning_choice=winning_option)
+
+    try:
+        transaction = perform_payout(bet=bet, winning_bets=winning_bets)
+    except InsufficientFunds:
+        raise
+
     bet.resolved = True
     bet.save()
 
     return transaction
-
-
-def resolve_date_bet(bet, winning_date):
-    """
-    Resolves a DateBet
-    :param bet: the bet...
-    :param winning_date:
-    :return:
-    """
-    from .models import DateBet
-    from datetime import date
-
-    assert isinstance(bet, DateBet)
-    assert isinstance(winning_date, date)
-
-    placed_bets = bet.placeddatebet_set.all()
-
-    winning_bets = find_winning_dates(placed_bets=placed_bets, winning_date=winning_date)
-
-    return winning_bets
 
 
 def find_winning_dates(placed_bets, winning_date):
@@ -340,4 +344,25 @@ def find_winning_dates(placed_bets, winning_date):
     for index in indices:
         winning_bets.append(placed_bets[index])
 
-    return indices
+    return winning_bets
+
+
+def find_winning_choices(placed_bets, winning_choice):
+    """
+    Finds the placed bets with the correct choice chosen
+    :param placed_bets: iterable of PlacedChoiceBet
+    :param winning_choice: Choice
+    :return: list of winning PlacedChoiceBets
+    """
+    from .models import Choice, PlacedChoiceBet
+
+    assert isinstance(winning_choice, Choice)
+
+    winning_bets = []
+
+    for placed_bet in placed_bets:
+        assert isinstance(placed_bet, PlacedChoiceBet)
+        if placed_bet.chosen == winning_choice:
+            winning_bets.append(placed_bet)
+
+    return winning_bets
