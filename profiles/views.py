fa02cb2d029caf410da2084206370df69391eb4c
Thames1990 <ThomasMohr1990@gmail.com>
2016-06-07 14:40:43 +0200

    Feedback isn't ordered by resolved anymore Revamped feedback resolving form in profile for superusers

--- profiles/views.py
+++ profiles/views.py
@@ -23,7 +23,7 @@ def profile(request):
     return render(request, 'profiles/profile.html', {
         'resolved_placed_choice_bets': resolved_bets['resolved_placed_choice_bets'],
         'resolved_placed_date_bets': resolved_bets['resolved_placed_date_bets'],
-        'feedback': Feedback.objects.all().order_by('resolved')
+        'feedback': Feedback.objects.filter(resolved=False)
     })
 
 
